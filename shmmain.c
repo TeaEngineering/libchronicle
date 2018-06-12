f81e5b7caa4300412badfb08dc5ab32d8308cabc
Chris Shucksmith <chris@bitmex.com>
2018-06-12 10:47:16 +0100

    cli tool allows poll and write support

--- shmmain.c
+++ shmmain.c
@@ -6,12 +6,13 @@
 // This is a stand-alone tool for replaying a queue for use with valgrind etc that are tricky to
 // operate within KDB, e.g. with
 // valgrind --leak-check=full -v ./native/obj/shmmain :java/queue
-int pflag = 0;
+int print_data = 0;
 
 K printxy(K x, K y) {
-	if (pflag) {
-		printf("x=%llu y=", x->j);
-		fwrite(kC(y), sizeof(char), y->n, stdout);
+	if (print_data) {
+		printf("%llu: ", x->j);
+		if (y->t == KC)
+			fwrite(kC(y), sizeof(char), y->n, stdout);
 		printf("\n");
 	}
 	return ki(5);
@@ -27,64 +28,83 @@ K kfrom_c_str(const char* s) { // k symbol from string?
 int main(const int argc, char **argv) {
 	int c;
 	opterr = 0;
-	int kflag = 0;
+	int kxflag = 0;
+	int verboseflag = 0;
+	int pollflag = 0;
+	char* append = NULL;
+	uint64_t index = 0;
 
-	while ((c = getopt(argc, argv, "pk")) != -1)
+	while ((c = getopt(argc, argv, "kdmi:va:p")) != -1)
 	switch (c) {
-		case 'p':
-			pflag = 1;
+		case 'd':
+			print_data = 1;
 			break;
 		case 'k':
-			kflag = 1;
+			kxflag = 1;
+			break;
+		case 'v':
+			verboseflag = 1;
+			break;
+		case 'i':
+			break;
+		case 'a':
+			append = optarg;
+			break;
+		case 'p':
+			pollflag = 1;
 			break;
 		case '?':
-			if (isprint (optopt)) {
-				fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-			} else {
-				fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
-			}
+			fprintf (stderr, "Unknown option `-%c'.\n", optopt);
 			return 1;
 		default:
-			abort();
+			printf("%c??",c);abort();
 	}
 
 	if (optind + 1 > argc) {
-		printf("Missing mandatory argument.\n Expected: %s [-p] [-i INDEX] :queue\n", argv[0]);
+		printf("Missing mandatory argument.\n Expected: %s [-k] [-d] [-m] [-i INDEX] [-v] [-a text] [-p] :queue\n", argv[0]);
+		printf("  -k expect kx format queue\n");
+		printf("  -d print data\n");
+		printf("  -m print metadata\n");
+		printf("  -i INDEX resume from index\n");
+		printf("  -v verbose mode\n");
+		printf("  -a TEXT write value text\n");
+		printf("  -p poll queue (rather than exit)");
 		exit(-1);
 	}
 
 	// what follows is translated q calls from shmipc.q
 	K dir = kss(argv[optind]);
-	K parser = kss(kflag ? "kx" : "text");
+	K parser = kss(kxflag ? "kx" : "text");
 	K r;
 	per(shmipc_init(dir, parser));
-	per(shmipc_debug((K)NULL));
-	per(shmipc_peek(dir));
 
 	K cb = dl(&printxy, 2);
-	K index = kj(0);
-	per(shmipc_tailer(dir,cb,index));
-	per(shmipc_debug((K)NULL));
+	K kindex = kj(index);
+	per(shmipc_tailer(dir,cb,kindex));
 	per(shmipc_peek(dir));
-	per(shmipc_debug((K)NULL));
 
-	printf("writing\n");
-	K msg = kfrom_c_str("message from c");
-    per(shmipc_append(dir,msg));
-	per(shmipc_debug((K)NULL));
+	if (verboseflag) {
+		per(shmipc_debug((K)NULL));
+	}
 
-	per(shmipc_peek(dir));
-	per(shmipc_debug((K)NULL));
+	if (append) {
+		printf("writing %s\n", append);
+		K msg = kfrom_c_str(append);
+		per(shmipc_append(dir,msg));
+		r0(msg);
+	}
 
-	printf("closing down\n");
+	while (pollflag) {
+		usleep(500*1000);
+		per(shmipc_peek(dir));
+	}
+
 	per(shmipc_close(dir));
-	per(shmipc_debug((K)NULL));
 
 	r0(dir);
 	r0(parser);
 	r0(cb);
-	r0(index);
-	r0(msg);
+	r0(kindex);
 
 	return 0;
 }
